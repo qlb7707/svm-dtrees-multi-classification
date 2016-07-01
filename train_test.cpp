7b10b7226a919fb6a29263771ad037b03b89687a
qlb7707 <770759450@qq.com>
2016-07-01 10:07:11 +0800

    1. fix bug inupt training data is wrong 2. add testing code 3. packaging using autotools

--- train_test.cpp
+++ train_test.cpp
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <fstream>
 #include <sstream>
+#include <unistd.h>
 #define FEAT_SMALL_IMAGE
 #ifdef FEAT_SMALL_IMAGE
 #define FEAT_SIZE 8*8
@@ -41,7 +42,7 @@ string int2str(int n)
     str = oss.str();
     return str;
 }
-void train_mnist_svm()
+void train_mnist_svm(int debug)
 {
     int n_img = count_dir_files("train_image");
     if(n_img <= 0)
@@ -50,7 +51,12 @@ void train_mnist_svm()
         return;
     }
     ifstream ifs;
+    ofstream ofs;
     ifs.open("train_label.txt");
+    if(debug)
+    {
+        ofs.open("train_debug.txt");
+    }
     if(!ifs)
     {
         cout<<"train_label.txt missing"<<endl;
@@ -63,7 +69,6 @@ void train_mnist_svm()
     trainLabel = Mat::zeros(n_img,1,CV_32SC1);
     for(int i = 0; i < n_img; i++)
     {
-//        cout<<"iter:"<< i <<endl;
         string line,name;
         int label;
         if(!getline(ifs,line))
@@ -80,16 +85,31 @@ void train_mnist_svm()
         {
             for(int k = 0; k < 8; k++)
             {
-                img_feat.data[j * 8 + k] = tmp.at<uchar>(j,k);
+                img_feat.at<float>(0,j * 8 + k) = tmp.at<uchar>(j,k);
             }
         }
         normalize(img_feat,img_feat);
+       if(debug) 
+       {
+            for(int j = 0; j < 8; j++)
+            {
+                for(int k = 0; k < 8; k++)
+                {
+                    ofs << img_feat.at<float>(0,j * 8 + k)<<" ";
+                }
+            }
+            ofs<<endl;
+       }
         memcpy(trainData.data + i * FEAT_SIZE * sizeof(float),img_feat.data,FEAT_SIZE * sizeof(float));
         trainLabel.at<unsigned int>(i,0) = label;
     }
 
     //start_training
     //
+    if(debug)
+    {
+        ofs.close();
+    }
     cout<<"start training"<<endl;
     CvSVM svm;
     CvSVMParams params;
@@ -105,9 +125,147 @@ void train_mnist_svm()
 
 }
 
-int main()
+void test_mnist_svm(int debug)
 {
-     train_mnist_svm();
+    ifstream ifs;
+    ofstream ofs;
+    string filename,line;
+    Mat img;
+    int label,res,cnt = 0,correct = 0;
+    ofs.open("predict_result.txt",ios::out);
+    cout<<"start testing..."<<endl;
+    ifs.open("test_label.txt",ios::in);
+    CvSVM svm;
+    if(access("svm_train.xml",F_OK))
+    {
+        cout<<"svm_train.xml missing"<<endl;
+        return;
+    }
+    svm.load("svm_train.xml");
+    if(!ifs)
+    {
+        cout<<"test_label.txt missing"<<endl;
+        return;
+    }
+
+    Mat tmp = Mat(8,8,CV_8UC1);
+    Mat img_feat = Mat(1,FEAT_SIZE,CV_32FC1);
+    while(getline(ifs,line))
+    {
+        istringstream iss(line);
+        iss >> filename >> label;
+        img = imread(filename.c_str(),CV_LOAD_IMAGE_GRAYSCALE);
+        if(!img.data)
+            continue;
+        resize(img,tmp,tmp.size());
+        for(int i = 0; i < 8; i++)
+        {
+            for(int j = 0; j < 8; j++)
+            {
+                img_feat.at<float>(0,i * 8 + j) = tmp.at<uchar>(i,j); 
+            }
+        }
+        normalize(img_feat,img_feat);
+        if(debug)
+        {
+            for(int i = 0; i < 8; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    ofs << img_feat.at<float>(0,i*8 +j)<<" ";
+                }
+            }
+        }
+        res = svm.predict(img_feat);
+        ofs<< filename <<" "<< res <<" "<< label<<endl;
+        if(res == label)
+        {
+            correct ++;
+        }
+        cnt ++;
+    }
+    ofs<<"accurcy:"<<1.0 * correct / cnt << "(" << correct <<"/"<<cnt<<")"<<endl;
+    ifs.close();
+    ofs.close();
+}
+
+int main(int argc,char *argv[])
+{
+    int train_flag = 0,test_flag = 0, debug_flag = 0;
+    string arg1,arg2,arg3;
+    if(argc >= 2)
+    {
+        arg1 = argv[1];
+        if(arg1 == "-t")
+        {
+            train_flag = 1;
+        }
+        else if(arg1 == "-d")
+        {
+            debug_flag = 1;
+        }
+        else if(arg1 == "-p")
+        {
+            test_flag = 1;
+        }
+        else
+        {
+            cout << "param " << arg1 << "not supported" << endl;
+            return -1;
+        }
+    }
+    if(argc >= 3)
+    {
+        arg2 = argv[2];
+        if(arg2 == "-t")
+        {
+            train_flag = 1;
+        }
+        else if(arg2 == "-d")
+        {
+            debug_flag = 1;
+        }
+        else if(arg2 == "-p")
+        {
+            test_flag = 1;
+        }
+        else
+        {
+            cout << "param " << arg1 << "not supported" << endl;
+            return -1;
+        }
+    }
+    if(argc == 4)
+    {
+        arg3 = argv[3];
+        if(arg3 == "-t")
+        {
+            train_flag = 1;
+        }
+        else if(arg3 == "-d")
+        {
+            debug_flag = 1;
+        }
+        else if(arg3 == "-p")
+        {
+            test_flag = 1;
+        }
+        else
+        {
+            cout << "param " << arg1 << "not supported" << endl;
+            return -1;
+        }
+    }
+
+
+    if(train_flag)
+    {
+        train_mnist_svm(debug_flag);
+    }
+    if(test_flag)
+    {
+        test_mnist_svm(debug_flag);     
+    }
    // cout<<count_dir_files("test_image")<<endl;
     return 0;
 }
